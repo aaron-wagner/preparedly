e2e44bb68dafb83be2a7e6cd855833eccee9580d
Aaron Wagner <aaron.wagner@austintexas.gov>
2014-10-31 12:17:26 -0500

    TFS class debug

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -51,8 +51,8 @@ class TFS
     )
 
     if response != nil
+      logger.info response.body.inspect
       json_response = JSON.parse(response.body)
-      logger.info json_response.inspect
       if json_response['results'].length > 0
         return json_response['results'][0]['attributes']['Pixel Value'].to_i
      else
